96a870a856b6d8d157b512d0681c6add5ef38ea7
James Pinder <jhpinder@gmail.com>
2020-08-03 00:03:52 -0400

    custom submit and choose video button

--- testUpload.php
+++ testUpload.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "hymn: " . $_POST['hymn'] . PHP_EOL . PHP_EOL;
+echo "hymn: " . $_POST['hymn'] . PHP_EOL;
 echo "name: " . $_POST['fullname'] . PHP_EOL;
+echo "filename: " . $_FILES['video']['name'] . " "; 
 
 ?>
