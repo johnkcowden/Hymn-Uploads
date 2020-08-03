2d044322b93ae7407a720b8098b52feae66a9666
James Pinder <jhpinder@gmail.com>
2020-08-02 21:57:43 -0400

    bootstrap fun

--- testUpload.php
+++ testUpload.php
@@ -1,29 +1,6 @@
 <?php
-require 'vendor/autoload.php';
 
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
+echo "hymn: " . $_POST['hymn'] . PHP_EOL . PHP_EOL;
+echo "name: " . $_POST['fullname'] . PHP_EOL;
 
-$bucket = 'choir.jhpinder.com';
-$keyname = 'secret_hymn_uploads/testfile';
-
-$s3 = new S3Client([
-   'version'     => 'latest',
-   'region'      => 'us-east-1',
-   'profile'     => 'secondProfile'
-]);
-
-try {
-   // Upload data.
-   $result = $s3->putObject([
-       'Bucket' => $bucket,
-       'Key'    => $keyname,
-       'Body'   => 'Hello, world!'
-   ]);
-
-   // Print the URL to the object.
-   echo $result['ObjectURL'] . PHP_EOL;
-} catch (S3Exception $e) {
-   echo $e->getMessage() . PHP_EOL;
-}
 ?>
